8af62785b9e9354b9d196691c390c896569c578a
Naqushab Neyazee <naqushab.neyazee24@gmail.com>
2017-12-18 16:45:53 +0530

    introduce delay in scraping

--- SearchEngineScrapy/SearchEngineScrapy/settings.py
+++ SearchEngineScrapy/SearchEngineScrapy/settings.py
@@ -30,10 +30,10 @@ DEPTH_LIMIT = 1
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
