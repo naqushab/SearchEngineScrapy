a878c578179a560f0d5f2cb97dc6d46564bdf608
Naqushab Neyazee <neyazee@adobe.com>
2019-01-07 20:25:00 +0530

    add filetype parameter to search for specific filetype

--- SearchEngineScrapy/SearchEngineScrapy/spiders/searchenginespider.py
+++ SearchEngineScrapy/SearchEngineScrapy/spiders/searchenginespider.py
@@ -12,11 +12,15 @@ class SearchEngineScrapy(Spider):
 
     searchQuery = None
     searchEngine = None
+    fileType = None
     selector = None
 
-    def __init__(self, searchQuery, searchEngine = "bing", pages = 3, *args, **kwargs):
+    def __init__(self, searchQuery, fileType, searchEngine = "bing", pages = 3, *args, **kwargs):
         super(SearchEngineScrapy, self).__init__(*args, **kwargs)
         self.searchQuery = searchQuery.lower()
+        self.fileType = fileType.lower()
+        if fileType is not None:
+            self.searchQuery = "{0} filetype:{1}".format(self.searchQuery, self.fileType)
         self.searchEngine = searchEngine.lower()
         self.pages = int(pages)
 
