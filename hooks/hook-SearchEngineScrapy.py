0ed626f879d987a651304b03939bad51c7224a8c
Naqushab Neyazee <neyazee@adobe.com>
2019-01-28 01:23:34 +0530

    remove uneceesary files from john's build

--- hooks/hook-SearchEngineScrapy.py
+++
@@ -1,4 +0,0 @@
-from PyInstaller.utils.hooks import collect_submodules, collect_data_files
-
-# This hooks the scrapy project 'SearchEngineScrapy' to import all submodules, change name to match scrapy project
-hiddenimports = (collect_submodules('SearchEngineScrapy'))
